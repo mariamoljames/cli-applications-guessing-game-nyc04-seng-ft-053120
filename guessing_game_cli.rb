d9f09bf941cac58de344e400b02c2a5eee47de35
Mariamol James <mariamol.james@gmail.com>
2020-06-21 11:58:45 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,6 +8,8 @@ def generate_random_number
   rand(7)
 end
 def compare(user_input,random)
+  if user_input.to_i==random
+    puts "You guessed the correct number!"
 def run_guessing_game
   random=rand(7)
   user_input=gets.chomp
