4b2ac136b128f63a8333df67b3b43762b24ce05e
Mariamol James <mariamol.james@gmail.com>
2020-06-21 12:16:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -12,10 +12,10 @@ def run_guessing_game
   user_integer=user_input.to_i
   random=generate_random_number
   if user_input.eql?("exit")
-    return "Goodbye!"
+    puts "Goodbye!"
   elsif user_integer==random
-    return "You guessed the correct number!"
+    puts "You guessed the correct number!"
   elsif user_integer!=random
-    return "Sorry! The computer guessed #{random}."
+    puts "Sorry! The computer guessed #{random}."
   end
 end
