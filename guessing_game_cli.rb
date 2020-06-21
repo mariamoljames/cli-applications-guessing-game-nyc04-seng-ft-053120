647de800b270e9635b2c269218fc98a807301c43
Mariamol James <mariamol.james@gmail.com>
2020-06-21 12:08:08 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,17 +7,6 @@ end
 def generate_random_number
   rand(7)
 end
-def compare(input,random)
-  user_input=input.to_i
-  if user_input==random
-    puts "You guessed the correct number!"
-  elsif user_input!=random
-    puts "Sorry! The computer guessed #{random}"
-  else
-    puts "Goodbye!"
-  end
-end
-
 def run_guessing_game
   prompt_user
   input=get_user_input
