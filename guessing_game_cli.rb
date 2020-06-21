3f11b607ea94c750abb32db743fffc7ec04f40d2
Mariamol James <mariamol.james@gmail.com>
2020-06-21 12:21:50 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,12 +1,4 @@
-def prompt_user
-  puts "Guess a number between 1 and 6"
-end
-def get_user_input
-  gets.chomp
-end
-def generate_random_number
-  rand(7)
-end
+
 def run_guessing_game
   user_input=gets.chomp
   user_integer=user_input.to_i
