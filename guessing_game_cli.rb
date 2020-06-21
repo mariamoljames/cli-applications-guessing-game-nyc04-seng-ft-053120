ebff7d9e37e8b49ae70c881f75be74c5c294f8b6
Mariamol James <mariamol.james@gmail.com>
2020-06-21 12:20:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,7 +8,7 @@ def generate_random_number
   rand(7)
 end
 def run_guessing_game
-  user_input=get_user_input
+  user_input=gets.chomp
   user_integer=user_input.to_i
   random=generate_random_number
   if user_input.eql?("exit")
